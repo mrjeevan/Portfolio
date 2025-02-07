ceba2f3906f84a872c104e472360ce3aacff264a
M R Jeevan <mrjeevan2000@gmail.com>
2025-02-07 12:03:49 +0530

    uncommented grid, seo optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,45 @@ export const metadata: Metadata = {
       "A personal portfolio showcasing my work, experience, and skills as a UI Developer.",
     images: ["/og.png"],
   },
+  twitter: {
+    creator: "@m_r_jeevan",
+    title: "Jeevan | Front-End Developer",
+    description:
+      "A personal portfolio showcasing my work, experience, and skills as a UI Developer.",
+    images: [
+      {
+        url: "https://mrjeevan.dev/og-image.jpg",
+        alt: "Jeevan's Portfolio Preview",
+      },
+    ],
+  },
+
+  keywords: [
+    "Js",
+    "Next.js",
+    "React",
+    "TypeScript",
+    "MERN",
+    "Tailwind",
+    "Redux Toolkit",
+    "Material-UI",
+    "Bootstrap",
+    "JavaScript",
+    "HTML",
+    "CSS",
+    "Responsive UI",
+    "Prototyping",
+    "Web Performance",
+    "Chart Libraries",
+    "SSG",
+    "ISG",
+    "SSR",
+    "PWA",
+    "Front-end Development",
+    "UI Components",
+    "User Experience",
+    "Web Applications",
+  ],
 };
 
 export default function RootLayout({
